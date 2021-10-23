fa8588424475bb693075d25755c79ea5773768a8
Felipe Bastos Nunes <felipebastos@ifce.edu.br>
2021-10-23 13:05:22 -0300

    Vuemodoro relativamente funcional :-)

---
+++ src/components/Clockmodoro.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="card-body time">
+      {{ display() }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Clockmodoro',
+  props: {
+    valor: Number,
+    status: Boolean,
+  },
+  methods: {
+    display() {
+      const min = Math.floor(Number(this.valor / 60)).toString().padStart(2, '0');
+      const sec = Number(this.valor % 60).toString().padStart(2, '0');
+      return `${min}:${sec}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+div.time {
+  font-size: 5em;
+  text-align: center;
+}
+</style>
---
+++ src/components/Menumodoro.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+      <div class="card-header">
+        <div class="row justify-content-between">
+          <div class="col-8">
+            <h1><span class="fas fa-pepper-hot"></span> {{appname}}</h1>
+          </div>
+          <div class="col-2">
+            <h2><span @click="colors" class="fas fa-palette"></span></h2>
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Menumodoro',
+  props: ['appname'],
+  methods: {
+    colors() {
+      this.$emit('swapcolors');
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
